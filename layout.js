793306f89c6ed8ae2173633b8ab863661deac614
hikaruna <hikaruna@users.noreply.github.com>
2020-08-17 05:32:59 +0900

    cssクライアントサイド指定機能

--- layout.js
+++ layout.js
@@ -6,6 +6,7 @@ const template = `
             <li><a href="/">top</a></li>
             <li><a href="/test">test</a></li>
             <li><a href="/hoge">hoge</a></li>
+            <li><a href="hoge?css=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2Fkognise%2Fwater.css%40latest%2Fdist%2Fdark.min.css">hogeのwater.cssを適応した版</a></li>
         </ul>
     </nav>
 </header>
@@ -17,7 +18,16 @@ const template = `
 </main>
 
 <aside>
-    asideです。広告とか。
+    <p>
+    これは404ページをハックして作ったspaのエントリーポイントです。
+    以下にlocationから導出したwebcomponentを配置することで擬似的なspaを実現します。
+    これにより、アプリケーションサーバも静的サイトジェネレータも使わずにgithubPageで動的にサイトを構築できないか実験してます。
+    </p>
+
+    <dl>
+        <dt>water.cssのライセンス</dt>
+        <dd>https://github.com/kognise/water.css/blob/master/LICENSE.md</dd>
+    </dl>
 </aside>
 
 <footer>
